6341b67a7d3f8338e56676a1577358b5fe1d3a70
Albert Kong <akong6@illinois.edu>
2019-07-20 23:05:02 -0700

    build date: 2019-07-20 23:05:02.591104

---
+++ precache-manifest.f9fd9490e7529c0ddeccf05735f43fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb7581c89284a75d3ac46fc9faabd74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a588b652a300b29b71b",
+    "url": "/static/css/main.a4223a87.chunk.css"
+  },
+  {
+    "revision": "d1de9be203afaf52bdbb",
+    "url": "/static/js/2.ed46f01d.chunk.js"
+  },
+  {
+    "revision": "9a588b652a300b29b71b",
+    "url": "/static/js/main.3965453e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
